7163400a3562e968c11480a3b3bc8266af7da6f3
Amanda Huang <robun@campus-119-084.ucdavis.edu>
2019-01-29 09:01:32 -0800

    beautified studio 1

--- studio1/script.js
+++ studio1/script.js
@@ -18,15 +18,15 @@ function processForm(evt) {
   var myMsg = document.querySelector('#myMsg')
   var msgSection = document.querySelector('#msgSection')
 
-  if (choose=='active') {
-    adjtwo='run around and play';
-    image.innerHTML='<img id="avatar" src="images/dog.svg" alt="dog avatar" width="250" height="250">'
-  } else if (choose=='lazy') {
-    adjtwo='sleep all day';
-    image.innerHTML='<img id="avatar" src="images/dogone.svg" alt="dog avatar" width="250" height="250">'
-  } else if (choose=='anxious') {
-    adjtwo='chew on your socks';
-    image.innerHTML='<img id="avatar" src="images/dogtwo.svg" alt="dog avatar" width="250" height="250">'
+  if (choose == 'active') {
+    adjtwo = 'run around and play';
+    image.innerHTML = '<img id="avatar" src="images/dog.svg" alt="dog avatar" width="250" height="250">'
+  } else if (choose == 'lazy') {
+    adjtwo = 'sleep all day';
+    image.innerHTML = '<img id="avatar" src="images/dogone.svg" alt="dog avatar" width="250" height="250">'
+  } else if (choose == 'anxious') {
+    adjtwo = 'chew on your socks';
+    image.innerHTML = '<img id="avatar" src="images/dogtwo.svg" alt="dog avatar" width="250" height="250">'
   }
 
   myMsg.innerHTML = '<span>hello!</span> <br> my name is ' + name + ' and i am a ' + adjone + ', ' + numb + ' year old pup. i love to ' + verb + ' with my ' + noun + '.' + ' my favorite thing to eat is ' + food + ' and i love to ' + adjtwo + ', but my most favorite thing to do is be with ' + yourname + '!';
@@ -38,5 +38,5 @@ function processForm(evt) {
 
 function resetForm() {
   msgSection.className = "hide";
-  image.innerHTML='<img id="avatar" src="images/vague.svg" alt="dog avatar" width="250" height="250">'
+  image.innerHTML = '<img id="avatar" src="images/vague.svg" alt="dog avatar" width="250" height="250">'
 }
